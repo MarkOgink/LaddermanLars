2674ea7231eda03a66c90ba84bace13dc9076149
MarkOgink <MarkOgink@DESKTOP-S792VDH>
2018-03-30 11:39:52 +0200

    Scherm gemaakt en .gitignore aangepast

--- src/main/java/nl/han/ica/leathermanlars/LeathermanLars.java
+++ src/main/java/nl/han/ica/leathermanlars/LeathermanLars.java
@@ -1,21 +1,41 @@
 package nl.han.ica.leathermanlars;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import nl.han.ica.OOPDProcessingEngineHAN.View.EdgeFollowingViewport;
+import nl.han.ica.OOPDProcessingEngineHAN.View.View;
+import nl.han.ica.leathermanlars.Player;
+import processing.core.PApplet;
 
 @SuppressWarnings("serial")
 public class LeathermanLars extends GameEngine{
+	private Player player;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PApplet.main(new String[]{"nl.han.ica.leathermanlars.LeathermanLars"});
 	}
 
 	@Override
 	public void setupGame() {
-		// TODO Auto-generated method stub
-		
+		int worldWidth=1024;
+        int worldHeight=683;
+        createObjects();
+		createViewWithViewport(worldWidth, worldHeight, 800, 800, 1.1f);
 	}
-
+	
+	private void createViewWithViewport(int worldWidth,int worldHeight,int screenWidth,int screenHeight,float zoomFactor) {
+        EdgeFollowingViewport viewPort = new EdgeFollowingViewport(player, (int)Math.ceil(screenWidth/zoomFactor),(int)Math.ceil(screenHeight/zoomFactor),0,0);
+        viewPort.setTolerance(50, 50, 50, 50);
+        View view = new View(viewPort, worldWidth,worldHeight);
+        setView(view);
+        size(screenWidth, screenHeight);
+        view.setBackground(loadImage("src/main/java/nl/han/ica/leathermanLars/media/Achtergrond.png"));
+    }
+	
+	private void createObjects() {
+        player = new Player(this);
+        addGameObject(player, 100, 100);
+    }
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
--- src/main/java/nl/han/ica/leathermanlars/Player.java
+++ src/main/java/nl/han/ica/leathermanlars/Player.java
@@ -1,5 +1,36 @@
 package nl.han.ica.leathermanlars;
 
-public class Player {
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 
+public class Player extends AnimatedSpriteObject{
+	final int size=25;
+	private final LeathermanLars world;
+
+	public Player(LeathermanLars world) {
+		super(new Sprite("src/main/java/nl/han/ica/leathermanlars/media/sheriff_ned_featured.png"), 1);
+		this.world = world;
+		setCurrentFrameIndex(0);
+        setFriction(0.05f);
+	}
+
+	@Override
+	public void update() {
+		if (getX()<=0) {
+            setxSpeed(0);
+            setX(0);
+        }
+        if (getY()<=0) {
+            setySpeed(0);
+            setY(0);
+        }
+        if (getX()>=world.getWidth()-size) {
+            setxSpeed(0);
+            setX(world.getWidth() - size);
+        }
+        if (getY()>=world.getHeight()-size) {
+            setySpeed(0);
+            setY(world.getHeight() - size);
+        }
+	}
 }
